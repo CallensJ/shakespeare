fe1c9d1fbcecf28dc74ac9f69d1160bdf9abaad1
CallensJ <callens.johan@protonmail.com>
2025-02-18 15:38:01 +0100

    first commit backend

---
+++ book-management-backend/src/main/java/dev/team/shakespear/ShakespearApplication.java
@@ -0,0 +1,13 @@
+package dev.team.shakespear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakespearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakespearApplication.class, args);
+	}
+
+}
---
+++ book-management-backend/src/test/java/dev/team/shakespear/ShakespearApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.team.shakespear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShakespearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
