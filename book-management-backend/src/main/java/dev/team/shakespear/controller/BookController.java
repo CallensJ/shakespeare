3437ff3c688cead3304e7d76ccd764fe21b149f9
CallensJ <callens.johan@protonmail.com>
2025-02-19 14:36:31 +0100

    added db properties

--- book-management-backend/src/main/java/dev/team/shakespear/controller/BookController.java
+++ book-management-backend/src/main/java/dev/team/shakespear/controller/BookController.java
@@ -1,8 +1,34 @@
 package dev.team.shakespear.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.team.shakespear.model.Book;
+import dev.team.shakespear.service.BookService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @RestController
+@RequestMapping("/books")
 public class BookController {
-    
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public BooksList<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+        @GetMapping("/{id}")
+    public Book getBookById(@PathVariable int id) {
+        return bookService.getBookById(id);
+    }
+
 }
