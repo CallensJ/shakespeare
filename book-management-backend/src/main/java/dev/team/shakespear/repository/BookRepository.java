ceb71d29d78263e5df66312d26e475d55b25337c
CallensJ <callens.johan@protonmail.com>
2025-02-19 13:50:30 +0100

    added files in respective directories

---
+++ book-management-backend/src/main/java/dev/team/shakespear/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package dev.team.shakespear.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+    
+}
---
+++ book-management-backend/src/main/java/dev/team/shakespear/service/BookService.java
@@ -0,0 +1,8 @@
+package dev.team.shakespear.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    
+}
